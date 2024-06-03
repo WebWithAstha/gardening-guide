abfc2e2914c11c06137545b5f2aa997875224802
WebWithAstha <lodhiastha85@gmail.com>
2024-06-03 23:00:06 +0530

    Handled minor errors

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='text-center text-[#919591] mt-40 font-black text-4xl'>About Us <i class="ri-emotion-happy-line"></i></div>
+    <div className='text-center text-[#919591] mt-40 font-black text-4xl'>About Us <i className="ri-emotion-happy-line"></i></div>
 
   )
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className='text-center text-[#919591] mt-40 font-black text-4xl'>Contact Us <i class="ri-hand"></i></div>
+    <div className='text-center text-[#919591] mt-40 font-black text-4xl'>Contact Us <i className="ri-hand"></i></div>
   )
 }
 
